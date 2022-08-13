f75f6319a40a5e756488a67e3daba5596398d5e2
SRIJAN-08 <101937323+SRIJAN-08@users.noreply.github.com>
2022-08-13 14:53:46 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -56,7 +56,7 @@ stages = ['''
 =========
 ''']
 end_of_game = False
-word_list = ["aardvark", "baboon", "camel"]
+word_list = ["panda", "wolverine", "camel", "hyenas", "cheetah", "crocodile", "rhinoceros", "cat", "dog"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 lives = 6
